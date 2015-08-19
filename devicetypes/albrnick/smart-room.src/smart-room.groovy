b13bdaa4671b972e89cb11aaef21351d8d890301
Nick Albright <nick.albright@gmail.com>
2015-08-18 22:27:45 -0400

    Covnerted "off" thermo state to "emergency heat" to allow "switch" type commands.

--- devicetypes/albrnick/smart-room.src/smart-room.groovy
+++ devicetypes/albrnick/smart-room.src/smart-room.groovy
@@ -28,15 +28,15 @@ metadata {
 	// hack
 	// heat -> Lights Changable & Occupied
     // cool -> Lights Changable & UnOccupied
-    // off -> Lights Not Changable & UnOccupied
+    // emergency heat -> Lights Not Changable & UnOccupied
     // auto -> Lights Not Changable & Occupied
 
 	tiles {
 		standardTile("thermostatMode", "device.thermostatMode", width: 2, height: 2, canChangeIcon: true ) {
-			state("heat", label: "a", action: "auto", icon: "st.doors.garage.garage-closed", backgroundColor: "#ffffff", nextState: "auto")
-			state("cool", label: "${device.label}", action: "off", icon: "st.doors.garage.garage-open", backgroundColor: "#ffffff", nextState: "off", defaultState: true)
-			state("off", label: "c", action: "cool", icon: "st.doors.garage.garage-open", backgroundColor: "#bbbbbb", nextState: "cool")
-			state("auto", label: "d", action: "heat", icon: "st.doors.garage.garage-closed", backgroundColor: "#bbbbbb", nextState: "heat")
+			state("heat", label: "occ heat", action: "auto", icon: "st.doors.garage.garage-closed", backgroundColor: "#ffffff", nextState: "auto")
+			state("cool", label: "${device.label}", action: "emergencyHeat", icon: "st.doors.garage.garage-open", backgroundColor: "#ffffff", nextState: "emergency heat", defaultState: true)
+			state("emergency heat", label: "c", action: "cool", icon: "st.doors.garage.garage-open", backgroundColor: "#bbbbbb", nextState: "cool")
+			state("auto", label: "occ auto", action: "heat", icon: "st.doors.garage.garage-closed", backgroundColor: "#bbbbbb", nextState: "heat")
 		}
 		standardTile("state", "device.state", width: 2, height: 2, canChangeIcon: true ) {
 			state("on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off")
@@ -71,7 +71,7 @@ def set_state( occupied, no_trigger_lights ) {
     	if ( occupied ) {
         	s = 'auto'
         } else {
-        	s = 'off'
+        	s = 'emergency heat'
         }        	
     } else {	// Triggerable lights!
     	if ( occupied ) {
@@ -110,13 +110,13 @@ def on() {
     tell_parent_auto_lights()
     
 }
-def off() { 
-	log.debug("off..2")
+def emergencyHeat() { 
+	log.debug("emergencyHeat..2")
 
 
     tell_parent_no_auto_lights()
 
-	sendEvent(name: 'thermostatMode', value: 'off', isStateChange: true)
+	sendEvent(name: 'thermostatMode', value: 'emergency heat', isStateChange: true)
 
 }
 
